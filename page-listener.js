bd0722d61f9e7d4d054d53046b9c931384e530d4
Bryan Wehner <bwehner@bwehner-mac.wifi.tripadvisor.com>
2017-07-24 14:34:47 -0400

    More page listener

--- page-listener.js
+++ page-listener.js
@@ -1,3 +1,9 @@
 (function () {
-    var port = chrome.runtime.connect({name: ""})
-})()
+    var port = chrome.runtime.connect({name: "main-page-events"});
+
+    chrome.tabs.onUpdated.addListener(
+        function(tabId, changeInfo, tab) {
+            debugger;
+        }
+    )
+})();
