10aff1f7a67df2ea59f519e2ba1d6459560ed984
Ethan Adams <eadams@tripadvisor.com>
2017-07-24 15:38:39 -0400

    progress

--- actions.js
+++ actions.js
@@ -23,6 +23,28 @@ function openNewTab() {
 		chrome.windows.update(window.id, leftWindowInfo)
 		chrome.windows.create(rightWindowInfo);
 	});
+	
+	var requestFilter = {
+		urls: [
+			"<all_urls>"
+		]
+	};
+
+	chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+		var headers = details.requestHeaders;
+		if( !localStorage['user-agent'] ) {
+			return;
+		}
+		for(var i = 0, l = headers.length; i < l; ++i) {
+			if( headers[i].name == 'User-Agent' ) {
+				break;
+			}
+		}
+		if(i < headers.length) {
+			headers[i].value = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
+		}
+		return {requestHeaders: headers};
+	}, requestFilter, ['requestHeaders','blocking']);
 }
 
 chrome.browserAction.onClicked.addListener(openNewTab);
