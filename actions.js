17a1e3a3ec02a728ad94bed87007334d02de6ca9
Bryan Wehner <bwehner@bwehner-mac.wifi.tripadvisor.com>
2017-07-25 13:45:35 -0400

    Added full window-mirroring to actions.js. Windows mirror tab creation, destruction, and navigation

--- actions.js
+++ actions.js
@@ -2,40 +2,95 @@
 // browser actions
 
 // Map from desktop tab id's to mobile tab id's
-let tabPairs = {}
+let tabPairs = {};
 
-function setOldTabId(oldTabId) {
-	return function (newWindow) {
-        tabPairs[oldTabId] = newWindow.tabs[0].id
-    }
-}
+// ID's of desktop (left) and mobile (right) windows
+let desktopWindowId;
+let mobileWindowId;
 
-function setDesktopTabId(desktopTabId) {
-	return (newWindow) => {
-        tabPairs[desktopTabId] = newWindow.tabs[0].id;
-    }
-}
+// Marks whether a new tab is currently being created, prevents "infinite loops" of tab creation
+let newTabBeingCreated = false;
 
 function getCorrespondingTab(tabId) {
-	let correspondingTab = tabPairs[tabId] || false;
+    let correspondingTab = tabPairs[tabId] || false;
 
-	Object.keys(tabPairs).forEach((key) => {
-		if (tabId === tabPairs[key]) {
-			correspondingTab = parseInt(key, 10);
-		}
-	})
+    if (!correspondingTab) {
+        Object.keys(tabPairs).forEach((key) => {
+            if (tabId === tabPairs[key]) {
+                correspondingTab = parseInt(key, 10);
+            }
+        });
+    }
 
-	return correspondingTab;
+
+    return correspondingTab;
 }
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	let correspondingTab = getCorrespondingTab(tabId)
+function MirrorLeftWindow(leftWindow, oldTabId) {
+	return function createAndMirrorRightWindow(rightWindow) {
+        tabPairs[oldTabId] = rightWindow.tabs[0].id;
+
+		desktopWindowId = leftWindow.id;
+		mobileWindowId = rightWindow.id;
+    }
+}
+
+// Listen to URL change in any tab and update the corresponding tab with the proper url
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	let correspondingTab = getCorrespondingTab(tabId);
 	if (changeInfo.url && correspondingTab) {
 		chrome.tabs.update(correspondingTab, {url: changeInfo.url});
 	}
-})
+});
 
-function openNewTab() {
+// Listen to tab creation and mirror it in the other window and update the tab map if relevant
+chrome.tabs.onCreated.addListener((newUserTab) => {
+	let newTabUrl = newUserTab.url;
+	if (!newTabBeingCreated && newUserTab.windowId === desktopWindowId) {
+		// Prevents infinite loop of tab creation
+        newTabBeingCreated = true;
+		chrome.tabs.create({windowId: mobileWindowId}, (newProgrammaticTab) => {
+			chrome.tabs.update(newProgrammaticTab.id, {url: newTabUrl}, (updatedProgrammaticTab) => {
+                // New tab is in desktop window, programmatic is mobile
+                tabPairs[newUserTab.id] = newProgrammaticTab.id;
+                newTabBeingCreated = false;
+			});
+		});
+	} else if (!newTabBeingCreated && newUserTab.windowId === mobileWindowId) {
+        newTabBeingCreated = true;
+        chrome.tabs.create({windowId: desktopWindowId}, (newProgrammaticTab) => {
+            // New tab is in mobile window, programmatic is desktop
+            chrome.tabs.update(newProgrammaticTab.id, {url: newTabUrl}, (updatedProgrammaticTab) => {
+                tabPairs[newProgrammaticTab.id] = newUserTab.id;
+                newTabBeingCreated = false;
+            });
+        });
+	}
+});
+
+// Listen to tab removal and mirror it in the other window and update the tab map if relevant
+chrome.tabs.onRemoved.addListener((removedTabId) => {
+	let removedTabIsDesktop = tabPairs[removedTabId] ? true : false;
+	let correspondingTab = getCorrespondingTab(removedTabId);
+    if (correspondingTab) {
+        if (removedTabIsDesktop) {
+            delete tabPairs[removedTabId];
+        } else {
+            delete tabPairs[correspondingTab];
+        }
+    	chrome.tabs.remove(correspondingTab);
+	}
+});
+
+// Listen to a change in which tab is focused in a window and update the focus of the tab in the corresponding window
+chrome.tabs.onActivated.addListener((activeInfo) => {
+	if (activeInfo.windowId === desktopWindowId || activeInfo.windowId === mobileWindowId) {
+		let correspondingTab = getCorrespondingTab(activeInfo.tabId);
+		chrome.tabs.update(correspondingTab, {selected: true});
+	}
+});
+
+function CreateMirroredWindow() {
 	chrome.windows.getCurrent((window) => {
 		// get the screen dimensions
 		// resize the current window
@@ -45,7 +100,7 @@ function openNewTab() {
 			left: 0,
 			width: screen.width / 2,
 			height: screen.height
-		}
+		};
 		const rightWindowInfo = {
 			top: 0,
 			left: screen.width / 2,
@@ -53,12 +108,12 @@ function openNewTab() {
 			height: screen.height,
 			focused: false,
 			url: 'http://tripadvisor.com'
-		}
-		chrome.windows.update(window.id, leftWindowInfo)
+		};
+		chrome.windows.update(window.id, leftWindowInfo);
 
         // create new window with info and callback
         chrome.tabs.query({active: true, currentWindow: true}, function(tabArray) {
-            chrome.windows.create(rightWindowInfo, setDesktopTabId(tabArray[0].id));
+            chrome.windows.create(rightWindowInfo, MirrorLeftWindow(window, tabArray[0].id));
         })
 	});
 
@@ -94,4 +149,4 @@ function openNewTab() {
 	}, requestFilter, ['requestHeaders','blocking']);
 }
 
-chrome.browserAction.onClicked.addListener(openNewTab);
+chrome.browserAction.onClicked.addListener(CreateMirroredWindow);
