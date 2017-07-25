160dddb21505acf0acd8e26dbbf3e2ba2e343b93
Ethan Adams <eadams@tripadvisor.com>
2017-07-25 13:49:45 -0400

    adding device sizes

--- actions.js
+++ actions.js
@@ -37,31 +37,31 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 function openNewTab() {
 	chrome.windows.getCurrent((window) => {
-		// get the screen dimensions
-		// resize the current window
-		// create the new window
-		const leftWindowInfo = {
-			top: 0,
-			left: 0,
-			width: screen.width / 2,
-			height: screen.height
-		}
-		const rightWindowInfo = {
-			top: 0,
-			left: screen.width / 2,
-			width: screen.width / 2,
-			height: screen.height,
-			focused: false,
-			url: 'http://tripadvisor.com'
-		}
-		chrome.windows.update(window.id, leftWindowInfo)
+		chrome.storage.local.get('device', (store) => {
+			const desktopWindowInfo = {
+				top: 0,
+				left: 0,
+				width: screen.width / 2,
+				height: screen.height
+			}
+			const mobileWindowInfo = {
+				top: 0,
+				left: screen.width / 2,
+				width: store.device ? store.device.width : screen.width / 2,
+				height: store.device ? store.device.height : screen.height,
+				focused: false,
+				url: 'http://tripadvisor.com'
+			}
 
-        // create new window with info and callback
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabArray) {
-            chrome.windows.create(rightWindowInfo, setDesktopTabId(tabArray[0].id));
-        })
+			// resize desktop window
+			chrome.windows.update(window.id, desktopWindowInfo)
+
+	        // create mobile window, add to map
+	        chrome.tabs.query({active: true, currentWindow: true}, (tabArray) => {
+	            chrome.windows.create(mobileWindowInfo, setDesktopTabId(tabArray[0].id));
+	        });
+		});
 	});
-
 
 	var requestFilter = {
 		urls: [
@@ -71,26 +71,28 @@ function openNewTab() {
 
 	// Listener to redirect to mobile pages
 	chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
-		var headers = details.requestHeaders;
-		// check if the tabId is a value in the map (a mobile page)
-		var found = false;
-		for (const key in tabPairs) {
-			if (tabPairs[key] == details.tabId) {
-				found = true
+		chrome.storage.local.get('device', (store) => {
+			var headers = details.requestHeaders;
+			// check if the tabId is a value in the map (a mobile page)
+			var found = false;
+			Object.keys(tabPairs).forEach((key) => {
+				if (tabPairs[key] == details.tabId) {
+					found = true
+				}
+			})
+			if (!found) {
+				return;
 			}
-		}
-		if (!found) {
-			return;
-		}
-		for(var i = 0, l = headers.length; i < l; ++i) {
-			if( headers[i].name == 'User-Agent' ) {
-				break;
+			for(var i = 0, l = headers.length; i < l; ++i) {
+				if( headers[i].name == 'User-Agent' ) {
+					break;
+				}
 			}
-		}
-		if(i < headers.length) {
-			headers[i].value = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) CriOS/27.0.1453.10 Mobile/10B350 Safari/8536.25';
-		}
-		return {requestHeaders: headers};
+			if(i < headers.length) {
+				headers[i].value = store.device.ua
+			}
+			return {requestHeaders: headers};
+		});
 	}, requestFilter, ['requestHeaders','blocking']);
 }
 
