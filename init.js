ea6e9b75986fca7a2a7c2c0f67549a2d24ed0834
Bryan Wehner <bwehner@bwehner-mac.wifi.tripadvisor.com>
2017-07-26 13:02:04 -0400

    Added local storage getters

--- init.js
+++ init.js
@@ -4,26 +4,41 @@ var storeDevice = (device) => {
 	if (!userAgents[device]) {
 		console.error('device does not exist');
 	}
-	localStorage['device'] = device
-	localStorage['ua'] = userAgents[device]['ua']
-	localStorage['width'] = userAgents[device]['width']
-	localStorage['height'] = userAgents[device]['height']
-}
+	localStorage['device'] = device;
+	localStorage['ua'] = userAgents[device]['ua'];
+	localStorage['width'] = userAgents[device]['width'];
+	localStorage['height'] = userAgents[device]['height'];
+};
 
 // "on" or "off"
 var storeScrollLock = (set) => {
 	localStorage['scrollLock'] = set;
-}
+};
 
 // "new" or "same"
 var storeStartSession = (set) => {
 	localStorage['startSession'] = set
-}
+};
 
 // "on" or "off"
 var storeGlueWindows = (set) => {
 	localStorage['glueWindows'] = set;
-}
+};
+
+// "on" or "off"
+var getScrollLock = () => {
+	return localStore['scrollLock'];
+};
+
+// "new" or "same"
+var getStartSession = (set) => {
+	return localStorage['startSession'];
+};
+
+// "on" or "off"
+var getGlueWindows = () => {
+	return localStorage['glueWindows'];
+};
 
 // runs when chrome extension is loaded
 let init = () => {
@@ -32,5 +47,5 @@ let init = () => {
 	storeScrollLock('on');
 	storeStartSession('new');
 	storeGlueWindows('on');
-}
+};
 init();
